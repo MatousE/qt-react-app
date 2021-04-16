9f610fbe41d15a23ecbb493fb9b420fa187d31ab
MatousE <matous.elphick@gmail.com>
2021-04-16 10:35:11 +0100

    feat: Adding functions for the encryption and decryption of the password

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app  = express();
 const mysql = require('mysql');
+const crypto = require('crypto');
+const algorithm = 'aes-256-ctr';
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
 
 const db = mysql.createPool({
     host: 'q-t-db.c8idrehbuuoy.eu-west-2.rds.amazonaws.com',
@@ -9,6 +13,21 @@ const db = mysql.createPool({
     database: 'qtdb'
 });
 
+function encrypt(text) {
+    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+}
+
+function decrypt(text) {
+    let iv = Buffer.from(text.iv, 'hex');
+    let encryptedText = Buffer.from(text.encryptedData, 'hex');
+    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
 
 
 app.get('/', (req,res)=>{
