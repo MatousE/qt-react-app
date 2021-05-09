d5371ad7cac9cf518fcd920a6f93d9920b8e540d
MatousE <matous.elphick@gmail.com>
2021-05-09 15:10:06 +0100

    feat: Updating back-end

--- server/index.js
+++ server/index.js
@@ -13,31 +13,19 @@ const db = mysql.createPool({
     database: 'qtdb'
 });
 
-function encrypt(text) {
-    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
-    let encrypted = cipher.update(text);
-    encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
-}
 
-function decrypt(text) {
-    let iv = Buffer.from(text.iv, 'hex');
-    let encryptedText = Buffer.from(text.encryptedData, 'hex');
-    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
-    let decrypted = decipher.update(encryptedText);
-    decrypted = Buffer.concat([decrypted, decipher.final()]);
-    return decrypted.toString();
-}
 
 
 app.get('/', (req,res)=>{
-    const sqlView = "SELECT * FROM qt_User";
-    const sqlInsert = "INSERT INTO qt_User (user_email,user_password) VALUES ('456@email.com','123123')"
+    // const sqlView = "SELECT * FROM user";
+    // const sqlInsert = "INSERT INTO user (email,password) VALUES ('mefda@email.com','12dasdf123')"
+    // db.query(sqlInsert, (err,result)=>{
+    //     res.send(result);
+    // });
+    //
+    // db.query(sqlView,(err,result,fields)=>{
+    //     res.send(result);
 
-    db.query(sqlView,(err,result,fields)=>{
-        if(err)throw err;
-        res.send(result);
-    })
 });
 
 app.listen(3001,()=>{
